c5d226b6645bae62069dd02ef4769c2852b04483
mel <peraltamdb@gmail.com>
2023-11-10 10:04:58 -0500

    simple, but slow? linear search

--- python-kata/course/search/linear_search.py
+++ python-kata/course/search/linear_search.py
@@ -6,4 +6,8 @@ from typing import List
 
 
 def linear_search(haystack: List[int], needle: int) -> bool:
-    ...
+    return bool(
+            len(
+                list(
+                    filter(lambda x: x == needle, haystack))))
+
